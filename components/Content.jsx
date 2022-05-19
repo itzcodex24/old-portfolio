0dbfd193e2bd5d455ec9761ec305cdd3662e6d81
Andrei <70026038+itzcodex24@users.noreply.github.com>
2022-05-19 16:26:10 +0100

    Update Content.jsx

--- components/Content.jsx
+++ components/Content.jsx
@@ -76,39 +76,6 @@ export default function Content() {
                               </motion.p>
                          </motion.div>
 
-//                          <motion.div
-//                               data-tilt
-//                               // data-tilt-reverse="true"
-//                               data-tilt-max="10"
-//                               data-tilt-scale="1.1"
-//                               className="font-text font-bold max-w-[1000px] bg-[#2E3F57] p-10 rounded-lg shadow-lg"
-//                               style={{
-//                                    transformStyle: "preserve-3d",
-//                                    transform: "perspective(200px)",
-//                               }}
-//                          >
-//                               <motion.p
-//                                    className="font-bold"
-//                                    style={{ transform: "translateZ(100px)" }}
-//                               >
-//                                    Lorem ipsum dolor sit amet consectetur,
-//                                    adipisicing elit. Inventore, autem dolorem
-//                                    architecto libero recusandae a aliquam
-//                                    debitis quod doloribus! Nihil reprehenderit
-//                                    rem eaque expedita similique incidunt enim
-//                                    magni sit doloremque! Corrupti accusantium
-//                                    tempore, rerum amet ab optio sunt. Numquam,
-//                                    porro? Repellat aut quidem adipisci fuga odit
-//                                    facere soluta molestiae, perferendis ut minus
-//                                    earum maiores consequatur, error minima sequi
-//                                    optio possimus? Ratione obcaecati odio,
-//                                    facere eligendi repellendus cum porro veniam
-//                                    vitae! Sequi ipsa harum aspernatur culpa
-//                                    magni illum molestiae dolor suscipit debitis,
-//                                    assumenda iure eligendi ducimus repellendus
-//                                    distinctio. Doloribus, porro eveniet!
-//                               </motion.p>
-//                          </motion.div>
 
                          <motion.h1
                               className="md:text-5xl text-3xl font-bold py-10 text-[#1bfaad] font-aboutme tracking-widest uppercase"
